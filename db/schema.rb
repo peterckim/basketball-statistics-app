4e816e86c45ac2a2171cd6d2e08f3c1ad3a2a3c8
peterckim <peterckim96@gmail.com>
2018-09-20 21:26:14 -0400

    added teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_001529) do
+ActiveRecord::Schema.define(version: 2018_09_21_011925) do
 
   create_table "games", force: :cascade do |t|
     t.string "home"
@@ -74,6 +74,12 @@ ActiveRecord::Schema.define(version: 2018_09_21_001529) do
 
   create_table "seasons", force: :cascade do |t|
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
