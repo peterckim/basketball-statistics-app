6bed810eef9b1d4828fc731b85f4748c2a5d89b3
peterckim <peterckim96@gmail.com>
2018-11-01 18:41:37 -0400

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?
-    private
+	helper_method :logged_in?, :current_user, :current_season
 
-    def logged_in?
-        !!session[:user_id]
-    end
+	private
 
-    def require_login
-        return head(:forbidden) unless session.include? :user_id
-    end
+	def logged_in?
+		!!session[:user_id]
+	end
+
+	def require_login
+		return head(:forbidden) unless session.include? :user_id
+	end
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if logged_in?
+	end
+
+	def current_season
+		Season.find_by(:year => 2018)
+	end
 end
