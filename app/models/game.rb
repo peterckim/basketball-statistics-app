24ae6ff517c482891e1875d5c9a1f980849bc06d
peterckim <peterckim96@gmail.com>
2018-11-01 18:48:26 -0400

    added association

--- app/models/game.rb
+++ app/models/game.rb
@@ -14,60 +14,61 @@
 #
 
 class Game < ApplicationRecord
-    NBA_TEAMS = [
-        "Atlanta Hawks",
-        "Boston Celtics",
-        "Brooklyn Nets",
-        "Charlotte Hornets",
-        "Chicago Bulls",
-        "Cleveland Cavaliers",
-        "Dallas Mavericks",
-        "Denver Nuggets",
-        "Detroit Pistons",
-        "Golden State Warriors",
-        "Houston Rockets",
-        "Indiana Pacers",
-        "Los Angeles Clippers",
-        "Los Angeles Lakers",
-        "Memphis Grizzlies",
-        "Miami Heat",
-        "Milwaukee Bucks",
-        "Minnesota Timberwolves",
-        "New Orleans Pelicans",
-        "New York Knicks",
-        "Oklahoma City Thunder",
-        "Orlando Magic",
-        "Philadelphia 76ers",
-        "Phoenix Suns",
-        "Portland Trail Blazers",
-        "Sacramento Kings",
-        "San Antonio Spurs",
-        "Toronto Raptors",
-        "Utah Jazz",
-        "Washington Wizards"
-    ]
+	NBA_TEAMS = [
+		"Atlanta Hawks",
+		"Boston Celtics",
+		"Brooklyn Nets",
+		"Charlotte Hornets",
+		"Chicago Bulls",
+		"Cleveland Cavaliers",
+		"Dallas Mavericks",
+		"Denver Nuggets",
+		"Detroit Pistons",
+		"Golden State Warriors",
+		"Houston Rockets",
+		"Indiana Pacers",
+		"Los Angeles Clippers",
+		"Los Angeles Lakers",
+		"Memphis Grizzlies",
+		"Miami Heat",
+		"Milwaukee Bucks",
+		"Minnesota Timberwolves",
+		"New Orleans Pelicans",
+		"New York Knicks",
+		"Oklahoma City Thunder",
+		"Orlando Magic",
+		"Philadelphia 76ers",
+		"Phoenix Suns",
+		"Portland Trail Blazers",
+		"Sacramento Kings",
+		"San Antonio Spurs",
+		"Toronto Raptors",
+		"Utah Jazz",
+		"Washington Wizards"
+	]
 
-    has_many :player_games
-    has_many :players, through: :player_games
+	has_many :player_games
+	has_many :players, through: :player_games
+	belongs_to :season
 
-    validates :home, presence: true
-    validates :home, inclusion: { in: NBA_TEAMS }
-    validates :away, presence: true
-    validates :away, inclusion: { in: NBA_TEAMS }
-    validates :date, presence: true
+	validates :home, presence: true
+	validates :home, inclusion: { in: NBA_TEAMS }
+	validates :away, presence: true
+	validates :away, inclusion: { in: NBA_TEAMS }
+	validates :date, presence: true
 
-    def create_player_game
-        game_service = GameService.new
-        player_game_hash = game_service.get_player_game_info(self.game_url)
+	def create_player_game
+		game_service = GameService.new
+		player_game_hash = game_service.get_player_game_info(self.game_url)
 
-        self.update(player_game_hash[:game])
+		self.update(player_game_hash[:game])
 
-        player_game_hash[:players].each do |player_hash|
-            player = Player.find_or_create_by(player_hash[:player])
-            player_game = PlayerGame.new(player_hash[:player_game])
-            player_game.player_id = player.id
-            player_game.game_id = self.id
-            player_game.save
-        end
-    end
+		player_game_hash[:players].each do |player_hash|
+			player = Player.find_or_create_by(player_hash[:player])
+			player_game = PlayerGame.new(player_hash[:player_game])
+			player_game.player_id = player.id
+			player_game.game_id = self.id
+			player_game.save
+		end
+	end
 end
