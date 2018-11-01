ecb9812eb8909f479e5929d63ccbac8a42e43681
peterckim <peterckim96@gmail.com>
2018-11-01 18:42:29 -0400

    added show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,23 +13,27 @@
 #
 
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
+	def new
+		@user = User.new
+	end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to seasons_path
-        else
-            render :new
-        end
-    end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to seasons_path
+		else
+			render :new
+		end
+	end
 
-    private
+	def show
+		@user = current_user
+	end
 
-    def user_params
-        params.require(:user).permit(:name, :email, :password)
-    end
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
+	end
 end
