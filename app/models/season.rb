f9e8153c840de1412b3b383c77a77907d1bb42ea
peterckim <peterckim96@gmail.com>
2018-11-01 18:47:28 -0400

    added relationship

--- app/models/season.rb
+++ app/models/season.rb
@@ -12,6 +12,7 @@ class Season < ApplicationRecord
     has_many :player_seasons
     has_many :player_season_projections
     has_many :players, through: :player_seasons
+    has_many :games
     
     def self.find_by_year(year)
         where("year = ?", year)
